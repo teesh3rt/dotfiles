79400f683d56f68f3205447795e7629b3a86ab3f
teesh3rt <ilaylevy611@gmail.com>
2026-01-29 22:19:34 +0200

    feat: add the gh command

---
+++ modules/base/gh.nix
@@ -0,0 +1,14 @@
+{ ... }:
+
+{
+  flake.modules.nixos.base = { pkgs, ... }: {
+    environment.systemPackages = [ pkgs.gh ];
+  };
+
+  flake.modules.homeManager.base = {
+    programs.gh = {
+      enable = true;
+      gitCredentialHelper.enable = true;
+    };
+  };
+}
