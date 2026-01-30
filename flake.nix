334b0cc99d37098d2ee2be7c25dc078dc8495185
teesh3rt <ilaylevy611@gmail.com>
2026-01-30 12:32:05 +0200

    feat: declare firefox with extensions
    should probably also declare unduck...

--- flake.nix
+++ flake.nix
@@ -19,6 +19,11 @@
       url = "github:notashelf/nvf";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ flake-parts, ... }:
--- modules/desktop/apps/firefox.nix
+++ modules/desktop/apps/firefox.nix
@@ -1,11 +1,25 @@
-{ ... }:
+{ inputs, config, ... }:
 
 {
   flake.modules.nixos.desktop = {
     programs.firefox.enable = true;
   };
 
-  flake.modules.homeManager.desktop = {
-    programs.firefox.enable = true;
+  flake.modules.homeManager.desktop = { pkgs, ... }: let
+    system = pkgs.stdenv.hostPlatform.system;
+    ff-addons = inputs.firefox-addons.packages.${system};
+  in {
+    programs.firefox = {
+      enable = true;
+      profiles.${config.flake.meta.user.name} = {
+        extensions.packages = with ff-addons; [
+          ublock-origin
+          sponsorblock
+          bitwarden
+          darkreader
+          youtube-shorts-block
+        ];
+      };
+    };
   };
 }
